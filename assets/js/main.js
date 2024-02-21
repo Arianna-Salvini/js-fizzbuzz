6ca4c0f743c5f25d5a080c0c6659dd2d80b9c471
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-21 16:54:16 +0100

    Add if-else condition to find multiple of 3 and replace them with "Frizz" otherwise if they're multiple of 5 than print "Buzz" and if they're multiple of both than print "FrizzBuzz"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 console.log("Fizz or Buzz?");
 
-for( let i=1; i < 101; i++){
+for (let i = 1; i < 101; i++) {
     console.log(i);
-}
+
+    if (i % 3 == 0){
+        console.log("Buzz")
+    } else if (i % 5 == 0){
+        console.log("Frizz");
+    } else if (i % 15 == 0) {
+        console.log("FrizzBuzz")
+    }
+};
