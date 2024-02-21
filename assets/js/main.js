cf59540f0736d80f6a1c56affb1762c9bcc659b0
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-21 20:06:12 +0100

    Fix list item innet text

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,35 @@
 console.log("Fizz or Buzz?");
 
 const container = document.getElementById("container")
+const ul = document.getElementById("unord_list")
+
 
 for (let i = 1; i <= 100; i++) {
 
-    let ul = document.createElement("ul")
-
-    container.append(ul)
-
-    let number = i
+    let li = document.createElement("li")
 
     if ((i % 3 == 0) && (i % 5 == 0)) {
         console.log("FizzBUzz")
-        number=("FizzBuzz")
+        li.innerHTML = "FizzBuzz"
+      
     } else if (i % 5 == 0) {
         console.log("Buzz");
-        number=("Buzz")
+        li.innerHTML = "Buzz"
+
+
 
     } else if (i % 3 == 0) {
         console.log("Fizz")
-        number=("Fizz")
+        li.innerHTML = "Fizz"
+     
+
     } else {
         console.log(i);
-        number=i
+        li.innerHTML = i
+    
     }
 
-    ul.insertAdjacentHTML("afterbegin", `<li> ${number} </li>`)
+    ul.append(li)
     ul.className="list_item"
 };
 
