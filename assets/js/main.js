66602771baf98498a348ac4bd93a4e92500e2011
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-21 19:03:19 +0100

    Fix list numer adding "Fizz" instead of multiple of 3 numbers, "Buzz" instead of multiple of 5 numbers and "FizzBuzz" insted of multiple of 3 and 5 numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,6 @@
 console.log("Fizz or Buzz?");
 
 const container = document.getElementById("container")
-let ul = document.createElement("ul")
-let il = document.createElement("il")
-
-container.append(ul)
-ul.append(il)
-
 
 for (let i = 1; i <= 100; i++) {
 
@@ -19,15 +13,15 @@ for (let i = 1; i <= 100; i++) {
     let number = i
 
     if ((i % 3 == 0) && (i % 5 == 0)) {
-        console.log("BuzzFizz")
-        number=i
+        console.log("FizzBUzz")
+        number=("FizzBuzz")
     } else if (i % 5 == 0) {
         console.log("Buzz");
-        number=i
+        number=("Buzz")
 
     } else if (i % 3 == 0) {
         console.log("Fizz")
-        number=i
+        number=("Fizz")
     } else {
         console.log(i);
         number=i
