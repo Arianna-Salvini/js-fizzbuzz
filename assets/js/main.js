c4981eb6814bd4024e82a805f8800a8b4db6dc30
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-21 18:57:21 +0100

    Add list number in DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,40 @@
 console.log("Fizz or Buzz?");
 
-for (let i = 1; i <= 100; i++) {
-
-    if ((i % 3 == 0) && (i % 5 == 0)){
-        console.log("BuzzFizz")
-
-    } else if (i % 5 == 0){
-        console.log("Buzz");
-              
-    } else if (i % 3 == 0) {
-          console.log("Fizz")
-    } else {
-        console.log(i);
-    }
-};
-
 const container = document.getElementById("container")
 let ul = document.createElement("ul")
 let il = document.createElement("il")
 
-
 container.append(ul)
 ul.append(il)
+
+
+for (let i = 1; i <= 100; i++) {
+
+    let ul = document.createElement("ul")
+    let il = document.createElement("il")
+
+    container.append(ul)
+    ul.append(il)
+
+    let number = i
+
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        console.log("BuzzFizz")
+        number=i
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+        number=i
+
+    } else if (i % 3 == 0) {
+        console.log("Fizz")
+        number=i
+    } else {
+        console.log(i);
+        number=i
+    }
+
+    il.append(number)
+};
 
 
 
